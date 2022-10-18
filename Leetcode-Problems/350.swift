7ee2260f8bdcc49d950fe00f322b3ffa8c4b71ba
Thomas Heinis <99673568+gemini-crocket@users.noreply.github.com>
2022-10-18 12:25:34 +0200

    350 Leetcode Problem Data Structure I study plan Intersection of Two Arrays II

---
+++ Leetcode-Problems/350.swift
@@ -0,0 +1,26 @@
+//
+//  350.swift : Intersection of Two Arrays II
+//  Leetcode-Problems
+//
+//  Created by Thomas Heinis on 18/10/2022.
+//
+
+class P350: Solution {
+  func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var map: [Int: Int] = [:]
+    var intersections: [Int] = []
+
+    for num in nums1 {
+      map[num, default: 0] += 1
+    }
+
+    for num in nums2 {
+      if map[num] != nil && map[num]! > 0 {
+        intersections.append(num)
+        map[num]! -= 1
+      }
+    }
+
+    return intersections
+  }
+}
