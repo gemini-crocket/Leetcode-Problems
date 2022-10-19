1de032c22a422961aafa6459a5ca10656dd55cc5
Thomas Heinis <99673568+gemini-crocket@users.noreply.github.com>
2022-10-19 11:31:00 +0200

    Problem 118 Leetcode Problem Data Structure I study plan Pascal’s Triangle

---
+++ Leetcode-Problems/118.swift
@@ -0,0 +1,34 @@
+//
+//  118.swift : Pascal’s Triangle
+//  Leetcode-Problems
+//
+//  Created by Thomas Heinis on 19/10/2022.
+//
+
+class P118: Solution {
+  func generate(_ numRows: Int) -> [[Int]] {
+    guard numRows != 0 else { return [] }
+    guard numRows != 1 else { return [[1]] }
+    guard numRows != 2 else { return [[1],[1,1]] }
+
+    var result: [[Int]] = [[1],[1,1]]
+
+    for i in 2..<numRows {
+      generateHelper(i, &result)
+    }
+
+    return result
+  }
+
+  func generateHelper(_ i: Int, _ result: inout [[Int]]) {
+    var row: [Int] = Array(repeating: 0, count: i + 1)
+    row[0] = 1
+    row[i] = 1
+
+    for j in 1 ..< i {
+      row[j] = result[i-1][j-1] + result[i-1][j]
+    }
+
+    result.append(row)
+  }
+}
