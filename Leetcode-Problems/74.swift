805dce052333d07805e90fdcc9e8cf76227732a9
Thomas Heinis <99673568+gemini-crocket@users.noreply.github.com>
2022-10-20 21:46:30 +0200

    Problem 74 Leetcode Problem Data Structure I study plan Search a 2D Matrix

---
+++ Leetcode-Problems/74.swift
@@ -0,0 +1,33 @@
+//
+//  74.swift : Search a 2D Matrix
+//  Leetcode-Problems
+//
+//  Created by Thomas Heinis on 20/10/2022.
+//
+
+class P74: Solution {
+  func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    guard !matrix.isEmpty else { return false }
+
+    let numberOfRows = matrix.count
+    let numberOfCols = matrix[0].count
+
+    var lowerLimit = 0
+    var upperLimit = numberOfRows * numberOfCols - 1
+
+    while lowerLimit <= upperLimit {
+      let middle = lowerLimit + (upperLimit - lowerLimit) / 2
+
+      let candidate = matrix[middle / numberOfCols][middle % numberOfCols]
+      guard candidate != target else { return true }
+
+      if candidate < target {
+        lowerLimit = middle + 1
+      } else {
+        upperLimit = middle - 1
+      }
+    }
+
+    return false
+  }
+}
